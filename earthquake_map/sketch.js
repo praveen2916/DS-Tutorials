1957fa4dca50df00f18aeeeea5def2c1964083b1
praveen2916 <dvpraveenkumar.2916@gmail.com>
2018-06-24 20:10:51 -0400

    locating tampa on 2d map

--- earthquake_map/sketch.js
+++ earthquake_map/sketch.js
@@ -1,8 +1,13 @@
 /// <reference path="./libraries/p5.global-mode.d.ts" />
 
+var w = 0;
+var h = 0;	
+
 var mapimg;
-
-
+//17.6868° N, 83.2185° E
+//27.9506° N, 82.4572° W
+var lat = 27.9506;
+var long = -82.4572;
 
 var url1 = 'https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/'
 var delim = ',';
@@ -15,18 +20,51 @@ var unknown = '0';
 var url2 = '?access_token=' 
 var api_key = config.API_KEY;
 
+var earthquakes; 
+
 
 function preload(){
-	var w = windowWidth;
-	var h = windowHeight;
+	var w = 1024;
+	var h = 512;
 	mapimg = loadImage(url1+clat+delim+clong+delim+zoom+delim+angle+delim+unknown+'/'+str(w)+'x'+str(h)+url2+api_key)
 	console.log(url1+clat+delim+clong+delim+zoom+delim+angle+delim+unknown+'/'+str(w)+'x'+str(h)+url2+api_key)
+}
+
+function mercX(long){
+	long = radians(long);
+	var a = (256/PI)*pow(2,int(zoom));
+	var b = long+PI;
+	return a*b;	
+}
+
+function mercY(lat){
+	lat = radians(lat);
+	var a = (256/PI)*pow(2,int(zoom));
+	var b = tan(PI/4 + lat/2);
+	var c = PI-log(b);
+	return a*c;
 }
 
 
 function setup() {
 	//createElement('h2','Earthquake Hotspots');
-	createCanvas(windowWidth,windowHeight);
+	createCanvas(1024,512);	
+	translate(width/2,height/2);
+	imageMode(CENTER);
 	image(mapimg,0,0);
+
+	var cx = mercX(int(clong));
+	var cy = mercY(int(clat));
+
+
+
+	var x = mercX(long)-cx;
+	var y = mercY(lat)-cy;
+
+	fill(255,0,255,200);
+	ellipse(x,y, 5,5);
+
+
+
 }
 
